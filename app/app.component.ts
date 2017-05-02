793db61ec3bda5fa56699a5343bc7e10f036cc07
Chris Carr and Megan Warnock <megan@>
2017-05-02 09:53:51 -0700

    set up initial file structure and keg class, constructor, and array

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-      <h1>Puppies!</h1>
+      <h1>Portland Tap Room</h1>
+      <ul>
+        <li *ngFor="let currentKeg of kegs"><strong>Name:</strong> {{currentKeg.name}},<strong>type of beer:</strong> {{currentKeg.type}}, $ {{currentKeg.price}}, <strong>alcohol content:</strong> {{currentKeg.alcoholContent}} </li>
+        </ul>
   </div>
   `
 })
 
 export class AppComponent {
+  kegs: Keg[] = [
+    new Keg("Mike Saw Sasquatch", "Blonde Ale", 5, 5),
+    new Keg("API", "American IPA", 7, 6)
+  ];
 
+}
+
+
+export class Keg {
+  public pints: number = 124;
+  constructor(public name: string, public type: string, public price: number, public alcoholContent: number) {}
 }
